0db96a33c9ea47c4938dcbcafd211c7f2f3c27e2
Randy Gulak <randy@tricano.com>
2023-04-27 18:22:59 -0700

    setup workshop and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import WorkShop from './pages/Workshop';
 
 function App() {
+
   return (
     <div id="app">
-    <h1>Hello!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/workshop' element={<WorkShop/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
