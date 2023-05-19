fe360b22bd69acd029b064f7c26dece6550b7b73
Randy Gulak <randy@tricano.com>
2023-05-18 19:46:23 -0700

    observer test

--- src/pages/Workshop.js
+++ src/pages/Workshop.js
@@ -1,18 +1,77 @@
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer, useState, useRef } from "react";
 import MediaQuery from "../components/MediaQuery";
 
 function WorkShop() {
+  const [isLarge, setIsLarge] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const { width } = entry.contentRect;
+        setIsLarge(width >= 400);
+      }
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+
   return (
-    <div>
-      <MediaQuery
-        query={"(max-width: 768px)"}
-        mobile={<h1>Mobile</h1>}
-        desktop={<h1>Desktop</h1>}
-        // Desktop Layout
-        // Mobile Layout
-      />
+    <div ref={ref}>
+      <div style={{ color: isLarge ? "red" : "black" }} className="test">
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio quae
+          veritatis quibusdam! Laboriosam delectus facilis excepturi dolor
+          numquam. Quos veritatis repudiandae nisi libero deleniti illum laborum
+          harum doloribus doloremque voluptas!
+        </h1>
+      </div>
     </div>
   );
 }
 
 export default WorkShop;
+
+// import React, { useEffect, useState, useRef } from 'react';
+
+// function MyComponent({ text }) {
+//   const [isLarge, setIsLarge] = useState(false);
+//   const ref = useRef(null);
+
+//   useEffect(() => {
+//     const observer = new ResizeObserver(entries => {
+//       for (let entry of entries) {
+//         const {width} = entry.contentRect;
+//         setIsLarge(width >= 600);
+//       }
+//     });
+
+//     if (ref.current) {
+//       observer.observe(ref.current);
+//     }
+
+//     return () => {
+//       if (ref.current) {
+//         observer.unobserve(ref.current);
+//       }
+//     };
+//   }, []);
+
+//   const wordLimit = isLarge ? 100 : 50;
+
+//   // Limit the words here as per your requirement
+
+//   return (
+//     <div ref={ref} className={isLarge ? 'large' : 'small'}>
+//       {text}
+//     </div>
+//   );
+// }
